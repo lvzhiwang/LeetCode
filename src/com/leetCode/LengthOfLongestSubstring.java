359059b8da4d5357f057dac46909a44b728e80d6
zhiwang <996074344@qq.com>
2017-12-08 16:30:37 +0800

    代码更新

--- src/com/leetCode/LengthOfLongestSubstring.java
+++ src/com/leetCode/LengthOfLongestSubstring.java
@@ -5,5 +5,6 @@ public class LengthOfLongestSubstring {
 	
 	public static void main(String[] args) {
 		System.out.println("============this is leetCode ================");
+		System.out.println("============this is leetCode ================");
 	}
 }
