f655f9eb4c4089bc2c1f5b6ee5e3c015d107b537
lvzhiwang <996074344@qq.com>
2018-10-14 21:45:14 +0800

    Update LengthOfLongestSubstring.java
    GitHub 进行文件修改

--- src/com/leetCode/LengthOfLongestSubstring.java
+++ src/com/leetCode/LengthOfLongestSubstring.java
@@ -6,7 +6,7 @@ public class LengthOfLongestSubstring {
 	public static void main(String[] args) {
 		System.out.println("============begin this is leetCode ================");
 		System.out.println("============end this is leetCode ================");
-		System.out.println("============begin this is leetCode ================");
-		System.out.println("============end this is leetCode ================");
+		System.out.println("============begin this is leetCode ================1");
+		System.out.println("============end this is leetCode ================2");
 	}
 }
