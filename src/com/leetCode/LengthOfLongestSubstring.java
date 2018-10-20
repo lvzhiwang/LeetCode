1393b816535675afff4cfa8d1e21ed7dd967371b
lvzhiwang <lvzhiwang@baidu.com>
2018-10-20 14:52:44 +0800

    code 1

--- src/com/leetCode/LengthOfLongestSubstring.java
+++ src/com/leetCode/LengthOfLongestSubstring.java
@@ -9,6 +9,6 @@ public class LengthOfLongestSubstring {
 		System.out.println("============begin this is leetCode ================1");
 		System.out.println("============end this is leetCode ================2");
 		System.out.println("============Add  ================2");
-		System.out.println("============commit  ================2");
+		System.out.println("============commit 1 ================2");
 	}
 }
